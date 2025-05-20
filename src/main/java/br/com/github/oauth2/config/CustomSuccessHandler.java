4d6cee9b484964a057c88b93ed10e2fb83767b32
Alan <aofrigerio@gmail.com>
2025-05-19 21:45:46 -0300

    Removido a parte de geração de token, pois não há necessidade nesse microsserviço

--- src/main/java/br/com/github/oauth2/config/CustomSuccessHandler.java
+++ src/main/java/br/com/github/oauth2/config/CustomSuccessHandler.java
@@ -1,13 +1,12 @@
 package br.com.github.oauth2.config;
 
-import br.com.github.oauth2.service.security.TokenService;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
@@ -16,21 +15,19 @@ import java.io.IOException;
 @Component
 public class CustomSuccessHandler implements AuthenticationSuccessHandler {
 
-
-    @Autowired
-    private TokenService tokenService;
-
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
 
+
         OAuth2AuthenticationToken oAuth2AuthenticationToken = (OAuth2AuthenticationToken) authentication;
-        String jwt = tokenService.gerarToken(oAuth2AuthenticationToken);
+        String jwtToken = ((DefaultOidcUser) oAuth2AuthenticationToken.getPrincipal()).getIdToken().getTokenValue();
 
         HttpSession session = request.getSession(false);
         if(session != null) session.invalidate();
 
         //TODO - INTERCEPTA O JWT E DEMONSTRA NA ROTA. PRECISA DA ROTA
-        response.sendRedirect("/auth/callback?token=" + jwt);
-
+        //RETORNA O JWT TOKEN DO PROVIDER DEFINIDO
+        //PODE ENVIAR AQUI NO AUTH SERVICE PARA PODER VALIDAR, PEGAR O ID SSO E VALIDAR COM A BASE, E APÓS ISSO, GERAR UM TOKEN INTERNO
+        response.sendRedirect("/auth/callback?token=" + jwtToken);
     }
 }
